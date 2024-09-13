cb91bbdb8a7b2ab65f3fef2dca53ff3dc711f823
Neeraj Patil <neeraj.patil@circolife.com>
2024-09-13 17:36:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',  // This binds the server to all network interfaces
-    port: 5173,       // Optionally specify the port
-    proxy: {
-      '/api': {       // Proxy /api requests
-        target: 'http://35.154.208.29:8080',  // Your backend API URL
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // Rewrite the path if necessary
-      }
-    }
+    host: '0.0.0.0',  // Bind to all network interfaces
+    port: 5173,       // Specify your port
+    https: false      // Ensure HTTPS is disabled
   }
 })
